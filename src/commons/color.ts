00bd00f91cf1a38c5c97f0560a002d4e2c33cf28
kobakakzu0429 <kobakazu10@gmail.com>
2019-05-02 06:16:44 +0900

    add ultraLightBlue

--- src/commons/color.ts
+++ src/commons/color.ts
@@ -2,6 +2,7 @@ const color = {
   red: "#f57173",
   blue: "#5a5ee7",
   lightBlue: "#8789ed",
+  ultraLightBlue: "#f6f6fe",
   gray: "#8F8DA5",
   lightGray: "#d2d3e3",
   ultraLightGray: "#fdfdff"
